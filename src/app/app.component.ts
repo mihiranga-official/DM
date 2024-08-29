ce34abf28725f8a2b686c0e67ad447fd8ecbd335
janith <mihiranga.office@gmail.com>
2024-08-29 18:02:19 +0530

    new login damaro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon'; 
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ScreenTheme {
+  private darkMode = false;
+
+  isDarkMode(): boolean {
+    return this.darkMode;
+  }
+
+  setDarkMode(isDarkMode: boolean): void {
+    this.darkMode = isDarkMode;
+    if (isDarkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, FormsModule,MatIconModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Practical-1';
+export class AppComponent implements OnInit {
+  
+  isDark!: boolean;
+  constructor(private screenTheme: ScreenTheme) {
+    this.isDark = this.screenTheme.isDarkMode();
+  }
+
+  ngOnInit(): void {}
+
+  onClickOnTheme(): void {
+    this.isDark = !this.isDark;
+    this.screenTheme.setDarkMode(this.isDark);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginFormComponent
+    },
+    { path: 'registration', 
+        component: RegisterFormComponent }
+];
