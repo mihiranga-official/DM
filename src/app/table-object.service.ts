664a4c4c20d6eb19dcd6a29f98fd42694920018e
janith <mihiranga.office@gmail.com>
2024-09-03 16:59:19 +0530

    comented

--- src/app/table-object.service.ts
+++ src/app/table-object.service.ts
@@ -2,12 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 
+
+function drt(){
+
+}
 @Injectable({
   providedIn: 'root'
 })
 export class TableObjectService {
-  baseUrl:string = 'assets/student.json';
-  private http:HttpClient = inject(HttpClient);//depedency injection re usebilty of code +
+  baseUrl:string = 'assets/student.json';//assign the HTTP url path
+  private http:HttpClient = inject(HttpClient);//depedency injection re usebilty of code +HttpClient angulr https calling methods
+
+ //method calling for gett  all student data via baseUrl
  getAllStudent():Observable<any[]>{
   return this.http.get<any[]>(this.baseUrl)
                   .pipe(
